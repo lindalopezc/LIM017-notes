ee132294c07d971113d4719b7584648cf0003cce
lindalopezc <lindalopezramos61@gmail.com>
2022-07-25 12:51:58 -0500

    layout login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,8 +2,21 @@ import React from 'react';
 
 const Login = () => {
     return (
-        <div>
-          <h1>Esta es la vista Login</h1>
+        <div className='login d-flex'>
+          <div>Aquí va un video </div> 
+          <div>
+              <form className='d-flex flex-column'>
+                <h1>Log In</h1>
+                <p>aqui va un texto</p>
+                <label for="email"/>Email:
+                <input type="email" id="email"/>
+                <label for="password"/>Password:
+                <input type="password" id="password"/>
+                <input type="button" value="Log In"/>
+                <input type="button" value="Sign in with Google"/>
+                <p>Don’t have an account yet? <span>Sign Up</span></p>
+              </form>
+          </div>
         </div>
     );
 }
