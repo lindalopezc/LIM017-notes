4e0c2a2993204124c5089186c622ac77e6db4e86
lindalopezc <lindalopezramos61@gmail.com>
2022-07-26 14:14:29 -0500

    control register component with react-hook

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const Register = () => {
+    const { register,handleSubmit } = useForm();
+    const onSubmit = (data) => {
+        console.log(typeof data);
+    }
     return (
-        <div>
-            <h1>Vista de register</h1>
+      <div className='register d-flex'>
+        <div>Aquí va un video </div> 
+          <div>
+              <form onSubmit = {handleSubmit(onSubmit)} className='d-flex flex-column'>
+                <h1>Sign Up</h1>
+                <p>aqui va un texto</p>
+								<label htmlFor="userName">Name: <input type="text" id="userName" {...register("userName")}/></label>
+                <label htmlFor="email">Email: <input type="text" id="email" {...register("email")}/></label>
+                <label htmlFor="password"/>Password:
+                <input type="password" id="password" {...register("password")}/>
+                <input type="submit" value="Sign up"/>
+                <input type="button" value="Sign in with Google"/>
+                <p>Don't have an account yet? <span>Sign In</span></p>
+              </form>
+          </div>
         </div>
     );
 }
