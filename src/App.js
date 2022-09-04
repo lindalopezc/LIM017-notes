67d85a0aa5edafead8094781ebdc2b3050474b98
lindalopezc <lindalopezramos61@gmail.com>
2022-09-03 19:48:22 -0500

    add google-login library

--- src/App.js
+++ src/App.js
@@ -5,20 +5,31 @@ import Register from '../src/components/Register';
 import Profile from './components/Profile';
 import RecycleBin from './components/RecycleBin';
 import Projects from "./components/Projects";
-import Project from "./components/Project";
 import Board from './components/Board';
+import { GoogleLogin } from 'react-google-login';
+
 
 function App() {
+  const responseGoogle = (response) => {
+    console.log(response);
+  }
   return (
     <div className="App">
+    <GoogleLogin
+    clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+    buttonText="Login"
+    onSuccess={responseGoogle}
+    onFailure={responseGoogle}
+    cookiePolicy={'single_host_origin'}
+    />,
+    
     <Routes>
-        <Route path="/" element={<Login />} exact/>
-        <Route path="/register" element={<Register/>} />
+        <Route path="/" element={<Register/>} exact/>
+        <Route path="/login" element={<Login/>} />
         <Route path="/login" element={<Login/>} />
         <Route path="/profile" element={<Profile/>} />
         <Route path="/recycle-bin" element={<RecycleBin/>} />
         <Route path="/projects" element={<Projects/>} />
-        <Route path="/project" element={<Project/>} />
         <Route path="/board" element={<Board/>} />
       </Routes>
     </div>
